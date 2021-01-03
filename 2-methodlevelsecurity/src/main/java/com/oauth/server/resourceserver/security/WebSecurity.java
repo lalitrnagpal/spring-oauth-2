b9ee6b563c21b2fc657f495f7a28d3b7d42d1f6c
Lalit Nagpal <lalit2801@gmail.com>
2021-01-03 16:42:42 +0530

    Spring OAuth 2.0 - Resource Server Example

---
+++ 2-methodlevelsecurity/src/main/java/com/oauth/server/resourceserver/security/WebSecurity.java
@@ -0,0 +1,37 @@
+package com.oauth.server.resourceserver.security;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+	private final Log logger = LogFactory.getLog(WebSecurityConfigurerAdapter.class);
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		this.logger.debug("Using our custom configure method of WebSecurity");
+		
+		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+		
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, "/users/status/check")
+			// .hasAuthority("SCOPE_profile")
+			.hasRole("developer")
+			//.hasAnyRole("developer", "user")
+			.anyRequest()
+			.authenticated()
+			.and()
+			.oauth2ResourceServer()
+			.jwt();
+//		http.formLogin();
+//		http.httpBasic();
+	}
+
+}
