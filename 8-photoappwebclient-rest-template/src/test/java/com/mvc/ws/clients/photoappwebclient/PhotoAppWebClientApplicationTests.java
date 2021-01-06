6fbe3187d8068a65544bc8179a3410af4bf43418
Lalit Nagpal <lalit2801@gmail.com>
2021-01-06 22:18:34 +0530

    OAuth, API Gateway, Eureka, Spring MVC Example

---
+++ 8-photoappwebclient-rest-template/src/test/java/com/mvc/ws/clients/photoappwebclient/PhotoAppWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvc.ws.clients.photoappwebclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppWebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
